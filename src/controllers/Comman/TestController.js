1c44cd055d7ba6586b3286aaf2e378b66668cb00
Ankit <66209504+ankit-maniya@users.noreply.github.com>
2024-07-14 13:47:54 -0600

    Test code removed

--- src/controllers/Comman/TestController.js
+++ src/controllers/Comman/TestController.js
@@ -8,42 +8,42 @@ import { model } from "../../models/index.js"
 const ObjectId = mongoose.Types.ObjectId
 
 const updateFunction = async (req, res, next) => {
-  const userId = new ObjectId("607b2629aca5b54a80cf4706")
+  // const userId = new ObjectId("607b2629aca5b54a80cf4706")
 
   try {
 
-    let partys = await getAllParty()
-    partys = partys || []
-    const partysLength = partys.length
-    if (partys && partysLength > 0) {
-      for (let i = 0; i < partysLength; i++) {
+    // let partys = await getAllParty()
+    // partys = partys || []
+    // const partysLength = partys.length
+    // if (partys && partysLength > 0) {
+    //   for (let i = 0; i < partysLength; i++) {
 
-        const cuttTypes = partys[i].cuttingType || []
-        const cuttTypesLength = cuttTypes.length
-        if (cuttTypes && cuttTypesLength > 0) {
+    //     const cuttTypes = partys[i].cuttingType || []
+    //     const cuttTypesLength = cuttTypes.length
+    //     if (cuttTypes && cuttTypesLength > 0) {
   
-          for (let j = 0; j < cuttTypesLength; j++) {
-            let type = cuttTypes[j].cutType
-            let loats = await getAllLoat(type, partys[i]._id)
+    //       for (let j = 0; j < cuttTypesLength; j++) {
+    //         let type = cuttTypes[j].cutType
+    //         let loats = await getAllLoat(type, partys[i]._id)
   
-            loats = loats || []
-            const loatsLength = loats.length 
-            for (let k = 0; k < loatsLength; k++) {
-              const date = loats[k].entryDate
-              const loatId = loats[k]._id
+    //         loats = loats || []
+    //         const loatsLength = loats.length 
+    //         for (let k = 0; k < loatsLength; k++) {
+    //           const date = loats[k].entryDate
+    //           const loatId = loats[k]._id
   
-              loats[k].month = await helper.getMonth(date) + 1
-              loats[k].year = await helper.getYear(date)
-              loats[k].cutId = cuttTypes[j]._id
+    //           loats[k].month = await helper.getMonth(date) + 1
+    //           loats[k].year = await helper.getYear(date)
+    //           loats[k].cutId = cuttTypes[j]._id
   
-              const loat = await updateLoat(loatId, loats[k])
-              // console.log('=-------loat-updated---===-=', loat);
-            }
-            // console.log('=-----------===-=', loats);
-          }
-        }
-      }
-    }
+    //           const loat = await updateLoat(loatId, loats[k])
+    //           // console.log('=-------loat-updated---===-=', loat);
+    //         }
+    //         // console.log('=-----------===-=', loats);
+    //       }
+    //     }
+    //   }
+    // }
 
     console.log("updated successfully!!")
     res.send(successRes("updated successfully!!"))
